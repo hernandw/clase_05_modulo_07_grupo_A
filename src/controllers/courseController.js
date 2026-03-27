e078e195753583495e772dc6c6bb63c861eeaaa4
Williams Hernández <hernandw@gmail.com>
2026-03-27 20:24:43 -0300

    :art: enlazar hojas de estilo

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -2,8 +2,20 @@
 
 const home  = (req, res)=>{
     try {
+
+        const courses = [
+            {
+                id: 5,
+                title:  "JS",
+                description: 'Buen curso',
+                price: 10
+            }]
+            
+        
         res.render('home',{
-            pageTitle: 'Inicio'
+            pageTitle: 'Inicio',
+            isExito: true,
+            courses
         })
     } catch (error) {
         console.log('Error al conectarse con el home', error)
