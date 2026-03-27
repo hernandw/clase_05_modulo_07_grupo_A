54fb96d273c9a40c44ebd79a9835300cb3791b84
Williams Hernández <hernandw@gmail.com>
2026-03-27 19:34:40 -0300

    :sparkles: confiración del servidor

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import exphbs from 'express-handlebars'
+import path from 'path'
+
+
+const __dirname = path.resolve()
+
+const app = express()
+
+const PORT = process.env.PORT || 3005
+
+
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+//static public
+app.use(express.static(path.join(__dirname, 'src/public')))
+
+//Sincronización de la BBDD
+
+//configuración de HBS
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'src/views'))
+
+app.engine('hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'src/views/layouts'),
+    extname: '.hbs'
+}))
+
+//rutas
+
+app.listen(PORT, ()=>{
+    console.log(`🚀 Servidor corriendo en el puerto http://localhost:${PORT}`)
+})
